95392c8b21f57b23ebb29b9a1ff08facfb103811
chescos <ch3sc0s@gmail.com>
2020-07-22 16:17:55 +0200

    added tailwind

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const tailwind = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    tailwind,
+    autoprefixer,
+  ],
+};
--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,23 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+      <div class="flex-shrink-0">
+        <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+      </div>
+      <div class="ml-6 pt-1">
+        <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
+        <p class="text-base text-gray-600 leading-normal">You have a new message!</p>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
+  @import './assets/css/app.css';
+
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
