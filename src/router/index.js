0944ccc0d149d86d9c4703709ad2bd8731d1a474
chescos <ch3sc0s@gmail.com>
2020-08-05 12:04:50 +0200

    added page titles

--- src/router/index.js
+++ src/router/index.js
@@ -17,16 +17,25 @@ const routes = [
     path: '/categories/:category',
     name: 'category',
     component: Category,
+    meta: {
+      title: 'Category',
+    },
   },
   {
     path: '/categories/:category/items/:item',
     name: 'item',
     component: Item,
+    meta: {
+      title: 'Item',
+    },
   },
   {
     path: '*',
     name: 'not-found',
     component: NotFound,
+    meta: {
+      title: 'Not Found',
+    },
   },
 ];
 
@@ -40,6 +49,19 @@ const router = new VueRouter({
 
     return { x: 0, y: 0 };
   },
+});
+
+const titleSuffix = 'CS:GO Skin Tester';
+
+router.afterEach((to) => {
+  // Use next tick to handle router history correctly.
+  Vue.nextTick(() => {
+    const { title } = to.meta;
+
+    document.title = title
+      ? `${title} | ${titleSuffix}`
+      : titleSuffix;
+  });
 });
 
 export default router;
