70a6013606c88bfbcf047420b0b4df8595060554
chescos <ch3sc0s@gmail.com>
2020-08-06 17:50:51 +0200

    improved 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,8 @@
 <template>
-  <div key="not-found">
-    <h1>Not found</h1>
+  <div class="flex h-screen -mt-32 -mb-20" key="not-found">
+    <div class="m-auto text-center">
+      <h1 class="text-6xl">404</h1>
+      <p>Whoops, looks like you are lost.</p>
+    </div>
   </div>
 </template>
